7c03b73aeda7aa8005b772e49600610c0f75ca97
Michael Zietz <michael.zietz@gmail.com>
2020-02-17 15:14:20 -0500

    Update compute file maps for N=4

--- scripts/1.compute_file_maps.py
+++ scripts/1.compute_file_maps.py
@@ -22,6 +22,9 @@ def get_subfiles(archive_file_path, n_drugs):
 
     For TWOSIDES, files are [drug_1, drug_2, file_type, subfile_name,
     archive_path]
+
+    For THREESIDES, files are [drug_1, drug_2, drug_3, file_type, subfile_name,
+    archive_path]
     """
     file_locations = list()
     try:
@@ -55,6 +58,15 @@ def get_subfiles(archive_file_path, n_drugs):
             drug_1, drug_2 = int(drug_1), int(drug_2)
             file_locations.append([drug_1, drug_2, file_type, subfile,
                                    archive_file_path.name])
+    elif n_drugs == 3:
+        for subfile in subfiles:
+            file_name_match = re.match(r'([a-z]+)(?:.*?__)([0-9]+)(?:_)([0-9]+)(?:_)([0-9]+)(?=\.npy)', subfile)
+            if not file_name_match:
+                raise ValueError(f'{archive_file_path.name} contained {subfile} not matched')
+            file_type, drug_1, drug_2, drug_3 = file_name_match.groups()
+            drug_1, drug_2, drug_3 = int(drug_1), int(drug_2), int(drug_3)
+            file_locations.append([drug_1, drug_2, drug_3, file_type, subfile,
+                                   archive_file_path.name])
     return file_locations
 
 
@@ -77,6 +89,7 @@ def compute_file_map(n_drugs, archives_path):
     n_drug_to_column_names = {
         1: ('drug', 'bootstrap'),
         2: ('drug_index_1', 'drug_index_2'),
+        3: ('drug_index_1', 'drug_index_2', 'drug_index_3')
     }
     column_names = [*n_drug_to_column_names[n_drugs], 'file_type', 'file_name',
                     'archive_file']
@@ -101,6 +114,12 @@ def compute_all_filemaps():
     twosides_file_map = compute_file_map(2, archives_path.joinpath('2/'))
     twosides_file_map.to_csv(meta_path.joinpath('file_map_twosides.csv'),
                              index=False)
+
+    # Compute and save THREESIDES file map
+    nsides_root = pathlib.Path('/data2/nsides/')
+    threesides_file_map = compute_file_map(3, nsides_root.joinpath('dddi/'))
+    threesides_file_map.to_csv(
+        nsides_root.joinpath('meta/file_map_twosides.csv'), index=False)
 
 
 if __name__ == "__main__":
